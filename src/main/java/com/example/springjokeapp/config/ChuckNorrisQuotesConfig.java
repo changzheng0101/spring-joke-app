7085315a6fe450b0a141031dfee5b7756b554bd3
Admin <11265585+tianjin-polytechnic-university_0@user.noreply.gitee.com>
2022-11-07 10:29:19 +0800

    java配置bean

---
+++ src/main/java/com/example/springjokeapp/config/ChuckNorrisQuotesConfig.java
@@ -0,0 +1,19 @@
+package com.example.springjokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author weixiao
+ * @date 2022/11/7 10:24
+ */
+@Configuration
+public class ChuckNorrisQuotesConfig {
+
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
